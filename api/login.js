14cf5e9d261a691e99aa15543bf0918f802e9c96
Vedantrp <92665493+Vedantrp@users.noreply.github.com>
2025-11-05 01:14:38 +0530

    Update login.js

--- api/login.js
+++ api/login.js
@@ -1,13 +1,19 @@
-export default async function handler(req, res) {
-  const redirect = process.env.SPOTIFY_REDIRECT_URI;
-  const scope = "user-read-email playlist-modify-private playlist-modify-public";
+export default function handler(req, res) {
+  const client_id = process.env.SPOTIFY_CLIENT_ID;
+  const redirect_uri = process.env.SPOTIFY_REDIRECT_URI;
+
+  const scopes = [
+    "user-read-email",
+    "playlist-modify-private",
+    "playlist-modify-public"
+  ].join("%20");
 
   const url =
     "https://accounts.spotify.com/authorize" +
-    `?client_id=${process.env.SPOTIFY_CLIENT_ID}` +
-    "&response_type=code" +
-    `&redirect_uri=${encodeURIComponent(redirect)}` +
-    `&scope=${encodeURIComponent(scope)}`;
+    `?response_type=code` +
+    `&client_id=${client_id}` +
+    `&redirect_uri=${encodeURIComponent(redirect_uri)}` +
+    `&scope=${scopes}`;
 
   res.redirect(url);
 }
