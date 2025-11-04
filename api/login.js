aa164948a34bdc96787731ca8f18ad5c8957adda
Vedantrp <92665493+Vedantrp@users.noreply.github.com>
2025-11-04 22:47:51 +0530

    Update login.js

--- api/login.js
+++ api/login.js
@@ -1,9 +1,8 @@
-export default async function handler(req, res) {
-  const client_id = process.env.SPOTIFY_CLIENT_ID;
+export default function handler(req, res) {
   const redirect_uri = "https://weather-tunes-kappa.vercel.app/api/callback";
+  const client_id = process.env.SPOTIFY_CLIENT_ID;
 
-  console.log("LOGIN --> redirect_uri:", redirect_uri);
-  console.log("LOGIN --> client_id:", client_id);
+  console.log("SPOTIFY LOGIN redirect ->", redirect_uri);
 
   const scope = [
     "playlist-modify-private",
@@ -13,12 +12,11 @@ export default async function handler(req, res) {
   ].join(" ");
 
   const authUrl =
-    `https://accounts.spotify.com/authorize?response_type=code` +
+    `https://accounts.spotify.com/authorize?` +
+    `response_type=code` +
     `&client_id=${client_id}` +
     `&redirect_uri=${encodeURIComponent(redirect_uri)}` +
     `&scope=${encodeURIComponent(scope)}`;
-
-  console.log("LOGIN --> authUrl:", authUrl);
 
   res.status(200).json({ authUrl });
 }
