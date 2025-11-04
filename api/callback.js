57a42edc58ee5c03b39f36118d0701164db98bf4
Vedantrp <92665493+Vedantrp@users.noreply.github.com>
2025-11-04 22:57:37 +0530

    Update callback.js

--- api/callback.js
+++ api/callback.js
@@ -24,6 +24,8 @@ export default async function handler(req, res) {
         redirect_uri: process.env.NEXT_PUBLIC_SPOTIFY_REDIRECT_URI,
         client_id: process.env.SPOTIFY_CLIENT_ID,
         client_secret: process.env.SPOTIFY_CLIENT_SECRET
+        redirect_uri: process.env.NEXT_PUBLIC_SPOTIFY_REDIRECT_URI,
+
       })
     });
 
