ebd2140fbc7ad2616209e55f455eaf231a6b84d7
Vedantrp <92665493+Vedantrp@users.noreply.github.com>
2025-11-05 13:06:39 +0530

    Update and rename generate-playlist.js to create-playlist.js

---
+++ api/create-playlist.js
@@ -0,0 +1,27 @@
+export default async function handler(req,res){
+  const { token, tracks } = req.body;
+
+  const me = await fetch("https://api.spotify.com/v1/me",{
+    headers:{ Authorization:`Bearer ${token}` }
+  }).then(r=>r.json());
+
+  const pl = await fetch(`https://api.spotify.com/v1/users/${me.id}/playlists`,{
+    method:"POST",
+    headers:{
+      Authorization:`Bearer ${token}`,
+      "Content-Type":"application/json"
+    },
+    body:JSON.stringify({ name:"WeatherTunes Basic", public:true })
+  }).then(r=>r.json());
+
+  await fetch(`https://api.spotify.com/v1/playlists/${pl.id}/tracks`,{
+    method:"POST",
+    headers:{
+      Authorization:`Bearer ${token}`,
+      "Content-Type":"application/json"
+    },
+    body: JSON.stringify({ uris:tracks })
+  });
+
+  res.json({ url: pl.external_urls.spotify });
+}
