aff4ed0bb07a8574a91011cb759c5da31a4f90a3
Vedantrp <92665493+Vedantrp@users.noreply.github.com>
2025-11-05 10:36:57 +0530

    Update login.js

--- api/login.js
+++ api/login.js
@@ -1,6 +1,7 @@
 export default function handler(req,res){
   const client = process.env.SPOTIFY_CLIENT_ID;
-  const redirect = encodeURIComponent(`${process.env.VERCEL_URL}/api/callback`);
+const redirect_uri = `${process.env.VERCEL_URL}/api/callback`;
+
   const scope = "user-read-email playlist-modify-private playlist-modify-public";
 
   const url = `https://accounts.spotify.com/authorize?client_id=${client}&response_type=code&redirect_uri=${redirect}&scope=${scope}`;
