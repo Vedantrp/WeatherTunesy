df261ef878e2f3c0d65f51e2789a7f62e00dd7f2
Vedantrp <92665493+Vedantrp@users.noreply.github.com>
2025-11-06 14:22:31 +0530

    Update callback.js

--- api/callback.js
+++ api/callback.js
@@ -2,7 +2,7 @@ import fetch from "node-fetch";
 
 export default async function handler(req, res) {
   const code = req.query.code;
-  const redirect = `${process.env.SPOTIFY_REDIRECT_URI}/api/callback`;
+  const redirect = `${process.env.SPOTIFY_REDIRECT_URI}`;
 
   const tokenRes = await fetch("https://accounts.spotify.com/api/token", {
     method: "POST",
