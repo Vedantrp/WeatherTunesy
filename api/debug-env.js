eaa9fafc198891a690d32bcacc46e40264b95791
Vedantrp <92665493+Vedantrp@users.noreply.github.com>
2025-11-06 15:53:37 +0530

    Update and rename debug-env.js to debug.js

--- api/debug-env.js
+++
@@ -1,6 +0,0 @@
-export default function handler(req, res) {
-  res.json({
-    SPOTIFY_REDIRECT_URI: process.env.SPOTIFY_REDIRECT_URI || null,
-    SPOTIFY_CLIENT_ID: process.env.SPOTIFY_CLIENT_ID ? "✅" : "❌",
-  });
-}
---
+++ api/debug.js
@@ -0,0 +1,7 @@
+
+export default function handler(req, res) {
+  res.json({
+    CLIENT_ID: process.env.SPOTIFY_CLIENT_ID ?? "❌ NULL",
+    REDIRECT: process.env.SPOTIFY_REDIRECT_URI ?? "❌ NULL"
+  });
+}
