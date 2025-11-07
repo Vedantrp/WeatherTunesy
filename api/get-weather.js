def733be9b1329400ce57736cb3d372aa13e0c3d
Vedantrp <92665493+Vedantrp@users.noreply.github.com>
2025-11-07 22:40:34 +0530

    Update get-weather.js

--- api/get-weather.js
+++ api/get-weather.js
@@ -1,15 +1,13 @@
 export default async function handler(req, res) {
-  const { city } = req.body;
   const key = process.env.WEATHER_API_KEY;
+  const { city } = req.body;
 
-  const r = await fetch(
-    `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${key}`
-  );
-  const j = await r.json();
+  const r = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&units=metric`);
+  const w = await r.json();
 
   res.json({
-    temp: j.main.temp,
-    feels_like: j.main.feels_like,
-    condition: j.weather[0].main
+    temp: w.main.temp,
+    feels_like: w.main.feels_like,
+    condition: w.weather[0].main
   });
 }
