1986aef27d2d117d182f9ac209ed1b8000e41d18
Vedantrp <92665493+Vedantrp@users.noreply.github.com>
2025-11-07 14:14:32 +0530

    Delete api/debug.js

--- api/debug.js
+++
@@ -1,8 +0,0 @@
-
-export default function handler(req, res) {
-  res.json({
-    CLIENT_ID: process.env.SPOTIFY_CLIENT_ID ?? "❌ NULL",
-    REDIRECT: process.env.SPOTIFY_REDIRECT_URI ?? "❌ NULL"
-  });
-}
- 
