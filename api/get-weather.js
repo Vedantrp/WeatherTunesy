9a9964861a7e5d333571deb7a38952151ea41d4b
Vedantrp <92665493+Vedantrp@users.noreply.github.com>
2025-11-25 19:26:44 +0530

    Update get-weather.js

--- api/get-weather.js
+++ api/get-weather.js
@@ -1,3 +1,4 @@
+export const dynamic ="force-dynamic";
 export default async function handler(req, res){
   try {
     if (req.method !== "POST") return res.status(405).json({ error: "POST only" });
