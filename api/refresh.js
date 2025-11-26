019597d54ed59e110a844dc02d9082345fd28c42
Vedantrp <92665493+Vedantrp@users.noreply.github.com>
2025-11-26 08:33:16 +0530

    Update refresh.js

--- api/refresh.js
+++ api/refresh.js
@@ -1,9 +1,7 @@
 module.exports = async (req, res) => {
   const { refresh } = req.body;
 
-  if (!refresh) {
-    return res.status(400).json({ error: "Missing refresh token" });
-  }
+  if (!refresh) return res.status(400).json({ error: "Missing refresh token" });
 
   const body = new URLSearchParams({
     grant_type: "refresh_token",
@@ -18,11 +16,11 @@ module.exports = async (req, res) => {
     body
   });
 
-  const token = await r.json();
+  const data = await r.json();
 
-  if (!token.access_token) {
-    return res.status(400).json({ error: "Failed to refresh token" });
+  if (!data.access_token) {
+    return res.status(400).json({ error: "Refresh failed", data });
   }
 
-  res.status(200).json({ token: token.access_token });
+  res.status(200).json({ token: data.access_token });
 };
