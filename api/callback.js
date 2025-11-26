e7078a34a1a1d1faa7f33e2cb2f97d422d047c1d
Vedantrp <92665493+Vedantrp@users.noreply.github.com>
2025-11-26 08:27:58 +0530

    Update callback.js

--- api/callback.js
+++ api/callback.js
@@ -1,7 +1,5 @@
-import { NextResponse } from "next/server";
-
-export async function GET(req) {
-  const code = req.nextUrl.searchParams.get("code");
+module.exports = async (req, res) => {
+  const code = req.query.code;
 
   const body = new URLSearchParams({
     grant_type: "authorization_code",
@@ -11,26 +9,27 @@ export async function GET(req) {
     client_secret: process.env.SPOTIFY_CLIENT_SECRET
   });
 
-  const tokenRes = await fetch("https://accounts.spotify.com/api/token", {
+  const r = await fetch("https://accounts.spotify.com/api/token", {
     method: "POST",
     headers: { "Content-Type": "application/x-www-form-urlencoded" },
     body
   });
 
-  const token = await tokenRes.json();
+  const token = await r.json();
 
   if (!token.access_token) {
-    return NextResponse.json({ error: "Invalid Code" }, { status: 400 });
+    return res.status(400).json({ error: "Invalid auth code" });
   }
 
   const userRes = await fetch("https://api.spotify.com/v1/me", {
     headers: { Authorization: `Bearer ${token.access_token}` }
   });
+
   const user = await userRes.json();
 
-  return NextResponse.json({
+  res.status(200).json({
     token: token.access_token,
     refresh: token.refresh_token,
     user
   });
-}
+};
